54a833f024925c22426e47dc55a9daac38eaa16d
Mark <markbochner1@gmail.com>
2024-08-09 12:52:48 -0700

    Updated with basic RAG functionality!

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import *
 import google.generativeai as genai
+from flask_pymongo import PyMongo
+from bson import ObjectId
 from uuid import uuid4
 import os
 import pickle
 from copy import deepcopy as copy
+from pymongo import MongoClient
+from langchain.vectorstores import MongoDBAtlasVectorSearch
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainExtractor
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain import hub
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
 
 from sqlalchemy.sql.functions import current_user
 
@@ -14,7 +25,20 @@ app.secret_key = 'your_secret_key_here'
 @app.before_request
 def startup():
     session['chat_hist'] = []
-
+    with open('mongo_info.txt') as f:
+        (user, password, url) = f.readlines()
+    mongo_uri = f'mongodb+srv://{user.strip()}:{password.strip()}@{url.strip()}/?retryWrites=true&w=majority&appName=website-database'
+    session['mongo_uri'] = mongo_uri
+    app.config["MONGO_URI"] = os.environ.get('MONGODB_URI', mongo_uri)
+    mongo = PyMongo(app)
+    embeddings = HuggingFaceEmbeddings()
+    vector_search = MongoDBAtlasVectorSearch.from_connection_string(
+        mongo_uri,
+        'website-database.education',
+        embeddings,
+        index_name="vec_ind"
+    )
+    app.retriever = vector_search.as_retriever(search_type="similarity", search_kwargs={"k": 5})
     app.before_request_funcs[None].remove(startup)
 
 
@@ -47,6 +71,31 @@ def chat():
         '''
         print('redir' + 'jobdesc=' + jobdesc)
         return jsonify({'response': 'Sure, here is Mark\'s resume', 'redirect_url': url_for('Resume'), 'type':2})
+    def discussEducation(query: str):
+        '''
+        Returns information about the Advanced Business Data Analytics program
+        Args:
+            query: A string, either containing the user's request or a slightly modified version of it if appropriate.
+        returns:
+            A response to the user's request
+        '''
+        google_api_key = open('google_api_key.txt').read()
+        client = MongoClient(session['mongo_uri'])
+        model = ChatGoogleGenerativeAI(model='gemini-1.5-flash', api_key=google_api_key)
+        prompt = hub.pull("rlm/rag-prompt")
+
+        def format_docs(docs):
+            return "\n\n".join(doc.page_content for doc in docs)
+
+        rag_chain = (
+                {"context": app.retriever | format_docs, "question": RunnablePassthrough()}
+                | prompt
+                | model
+                | StrOutputParser()
+        )
+        r = ' '.join([chunk for chunk in rag_chain.stream(query)])
+        return jsonify({'response': r, 'type':1})
+
     model = genai.GenerativeModel('gemini-1.5-flash-latest', safety_settings=[
             {
                 "category": "HARM_CATEGORY_HARASSMENT",
@@ -64,7 +113,7 @@ def chat():
                 "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
                 "threshold": "BLOCK_NONE"
             },
-        ], tools=[showResume])
+        ], tools=[showResume, discussEducation])
     hist = session.get('chat_hist')
     chat = model.start_chat(history=hist)
     message = request.json['message']
@@ -77,6 +126,9 @@ def chat():
         return jsonify({'response': response.text, 'type': 1})
     else:
         fn_call = response.parts[0].function_call.name
+        args = {}
+        for key in list(response.parts[0].function_call.args.keys()):
+            args[key] = response.parts[0].function_call.args[key]
         h = copy(chat.history[0])
         chat.history[-1] = h
         chat.history[-1].parts[0].text = 'Sure, here is Mark\'s Resume'
@@ -85,7 +137,7 @@ def chat():
                                  'parts': [{'text': part.text}
                                            for part in msg.parts]}
                                 for msg in chat.history]
-        return locals()[fn_call](jobdesc='asfd')#Figure out how to pull args
+        return locals()[fn_call](**args)#Figure out how to pull args
 
 @app.route('/get_chat_history', methods=['GET'])
 def get_chat_history():
