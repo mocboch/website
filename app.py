b4ffd09db39667ff6c22e9947138f222f73407eb
Mark <markbochner1@gmail.com>
2024-08-10 06:31:51 -0700

    added sendMemo, setInterview, and goHome functionality.

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask_pymongo import PyMongo
 from bson import ObjectId
 from uuid import uuid4
 import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 import pickle
 from copy import deepcopy as copy
 from pymongo import MongoClient
@@ -15,7 +17,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain import hub
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
-
+import threading
 from sqlalchemy.sql.functions import current_user
 
 app = Flask(__name__)
@@ -23,30 +25,36 @@ app.secret_key = 'your_secret_key_here'
 
 
 @app.before_request
-def startup():
-    session['chat_hist'] = []
+def startup():                                                                      #Sets up chat and vectorstore on startup
+    app.retriever_ready = False
+    session['chat_hist'] = [{'role': 'model',
+                             'parts': [{'text': 'Hi there! \nI\'m Mr. Botner, Mark\'s AI assistant. I can show you his resume, tell you about his skills, or get you in touch with him. I can even help you set up an interview! \n\nWhat can I do for you today?'}]}]                                                       #chat_hist is stored as a list of dicts in session memory
     with open('mongo_info.txt') as f:
-        (user, password, url) = f.readlines()
+        (user, password, url) = f.readlines()                                       #Assemble the mongo connection URI
     mongo_uri = f'mongodb+srv://{user.strip()}:{password.strip()}@{url.strip()}/?retryWrites=true&w=majority&appName=website-database'
-    session['mongo_uri'] = mongo_uri
+    session['mongo_uri'] = mongo_uri                                                #Store it in session memory
     app.config["MONGO_URI"] = os.environ.get('MONGODB_URI', mongo_uri)
-    mongo = PyMongo(app)
-    embeddings = HuggingFaceEmbeddings()
-    vector_search = MongoDBAtlasVectorSearch.from_connection_string(
-        mongo_uri,
-        'website-database.education',
-        embeddings,
-        index_name="vec_ind"
-    )
-    app.retriever = vector_search.as_retriever(search_type="similarity", search_kwargs={"k": 5})
+    mongo = PyMongo(app)                                                            #Configure and run PyMongo
+
+    def create_retriever(mongo_uri=mongo_uri):
+        embeddings = HuggingFaceEmbeddings()                                            #Instantiate a HuggingFaceEmbeddings model- this is taking too long
+        vector_search = MongoDBAtlasVectorSearch.from_connection_string(
+            mongo_uri,
+            'website-database.education',                                               #Create a vector search object
+            embeddings,
+            index_name="vec_ind"
+        )
+        app.retriever = vector_search.as_retriever(search_type="similarity", search_kwargs={"k": 10})    #Store it as a retriever for use later
+        app.retriever_ready = True
+    threading.Thread(target=create_retriever, daemon=True).start()
     app.before_request_funcs[None].remove(startup)
 
 
-@app.route('/')
+@app.route('/')                                             #Home page- robot image and chatbar
 def home():
 
     return render_template('home.html')
-@app.route('/Resume', methods=['GET'])
+@app.route('/Resume', methods=['GET'])              #Dynamic resume page
 def Resume():
     return render_template('Resume.html')
 
@@ -56,11 +64,39 @@ def handle_refresh():
     session['chat_hist'] = []
     return home()
 '''
+@app.route('/handleMemo', methods=['POST'])
+def handleMemo():
+    message = request.json['message']
+    session['chat_hist'].append([
+        {'role': 'user',
+         'parts': {'text': message}},
+        {'role': 'model',
+         'parts': {'text': 'Ok, I\'ll let Mark know. What else can I help you with?'}}
+    ])
+    msg = Mail(
+        from_email='deskofmarkbotner@gmail.com',
+        to_emails='markbochner1@gmail.com',
+        subject='Message from Mr. Botner!',
+        plain_text_content=message
+    )
+    sg = SendGridAPIClient(open('Twilio.txt').readlines()[0].strip())
+    response = sg.send(msg)
+
+
 
 @app.route('/chat', methods=['POST'])
-def chat():
-    with open('google_api_key.txt') as f:
-        genai.configure(api_key=f.read())
+def chat():                                         #Chat response logic
+    with open('google_api_key.txt') as f:           #Configure API key for google gemini
+        genai.configure(api_key=f.read())           #showResume returns a stock messsage and reroutes to the resume page
+                                                 #It also updates the session[chat_hist] to give the model the appropriate context
+    def goHome():
+        '''
+        Returns the user to the homepage. Called when the user requests to return to the home page.
+        Returns:
+            rendered template
+        '''
+        session['chat_hist'][-1]['parts'][0]['text'] = 'No problem. Is there anything else I can help you with today?'
+        return jsonify({'response': 'No problem. Is there anything else I can help you with today?', 'redirect_url': url_for('home'), 'type': 2})
     def showResume(jobdesc: str):
         '''
         Shows Mark's Resume page to any user who requests it.
@@ -70,7 +106,30 @@ def chat():
             rendered template
         '''
         print('redir' + 'jobdesc=' + jobdesc)
+        session['chat_hist'][-1]['parts'][0]['text'] = 'Sure, here is Mark\'s resume'
         return jsonify({'response': 'Sure, here is Mark\'s resume', 'redirect_url': url_for('Resume'), 'type':2})
+
+    #discussEducation is currently called very narrowly for the ABDA for testing purposes.
+    #It searches documents related to my education and returns them as context
+    #Also updates chat history for the model
+    def sendMemo():
+        '''
+        Leaves a memo for Mark. Called when the user requests contact info or to get in touch with Mark.
+        Returns:
+            Sends Mark a note
+        '''
+        session['chat_hist'][-1]['parts'][0]['text'] = 'Sure, I can definitely take a note for Mark! Go ahead and leave your message below, and I\'ll pass it along.'
+        return jsonify({'response': 'Sure, I can definitely take a note for Mark! '
+                                    'Go ahead and leave your message below, and I\'ll pass it along.', 'type':3})
+
+    def setInterview():
+        '''
+        Sets an interview with Mark. Called when the user requests to speak with Mark or inquires about his schedule.
+        Returns:
+            a calendly interface to interact with the customer
+        '''
+        session['chat_hist'][-1]['parts'][0]['text'] = 'I can set up a meeting with Mark- Have a look at his calendar and let me know what works for you.'
+        return jsonify({'response': 'I can set up a meeting with Mark- Have a look at his calendar and let me know what works for you.', 'type': 4})
     def discussEducation(query: str):
         '''
         Returns information about the Advanced Business Data Analytics program
@@ -94,8 +153,11 @@ def chat():
                 | StrOutputParser()
         )
         r = ' '.join([chunk for chunk in rag_chain.stream(query)])
+        session['chat_hist'][-1]['parts'][0]['text'] = r
+        print(query)
         return jsonify({'response': r, 'type':1})
-
+    tools = [showResume, sendMemo, setInterview, goHome]
+    if app.retriever_ready == True: tools.append(discussEducation)
     model = genai.GenerativeModel('gemini-1.5-flash-latest', safety_settings=[
             {
                 "category": "HARM_CATEGORY_HARASSMENT",
@@ -113,34 +175,35 @@ def chat():
                 "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
                 "threshold": "BLOCK_NONE"
             },
-        ], tools=[showResume, discussEducation])
-    hist = session.get('chat_hist')
-    chat = model.start_chat(history=hist)
-    message = request.json['message']
-    response = chat.send_message(message)
-    if len(response.parts[0].text) > 0:
-        session['chat_hist'] = [{'role': msg.role,
+        ], tools=tools)                                                     #Instantiate a model with tools
+    hist = session.get('chat_hist')                                         #Get the chat history, which can be fed to the model as a list of dicts
+    chat = model.start_chat(history=hist)                                   #Start a chat with the model and history
+    message = request.json['message']                                       #Gets the message (prompt) from the front end
+    response = chat.send_message(message)                                   #Calls gemini for a response to the prompt
+    if len(response.parts[0].text) > 0:                                     #Returns a text based response directly to the front end
+        session['chat_hist'] = [{'role': msg.role,                          #Serializes the chat history as a list of dicts and puts it away in the session storage
                                  'parts': [{'text': part.text}
                                            for part in msg.parts]}
                                 for msg in chat.history]
         return jsonify({'response': response.text, 'type': 1})
-    else:
-        fn_call = response.parts[0].function_call.name
+    else:                                                                   #else handles function calls
+        fn_call = response.parts[0].function_call.name                      #gets the name of the function called
         args = {}
-        for key in list(response.parts[0].function_call.args.keys()):
+        for key in list(response.parts[0].function_call.args.keys()):       #serializes the arguments from gemini as a list
             args[key] = response.parts[0].function_call.args[key]
-        h = copy(chat.history[0])
-        chat.history[-1] = h
-        chat.history[-1].parts[0].text = 'Sure, here is Mark\'s Resume'
+        h = copy(chat.history[0])                                           #Makes a deepcopy of the first message, which is user submitted and therefore text
+        chat.history[-1] = h                                                #Uses that as a blank to fill in the appropriate response once it's generated
+        chat.history[-1].parts[0].text = ' '
         chat.history[-1].role = 'model'
-        session['chat_hist'] = [{'role': msg.role,
+        session['chat_hist'] = [{'role': msg.role,                          #Serialize and store the chat history
                                  'parts': [{'text': part.text}
                                            for part in msg.parts]}
                                 for msg in chat.history]
-        return locals()[fn_call](**args)#Figure out how to pull args
+
+        return locals()[fn_call](**args)                                    #return the function call by calling it by name from locals
 
 @app.route('/get_chat_history', methods=['GET'])
-def get_chat_history():
+def get_chat_history():                                                     #route to update chat history after changing pages
     hist = session.get('chat_hist', [])
     return jsonify({'history': hist})
 
---
+++ static/chatbot.js
@@ -0,0 +1,75 @@
+
+var seq = 0
+function loadChatHistory() {
+
+    $.get('/get_chat_history', function(data) {
+        data.history.forEach(function(message) {
+            if (message.role === 'model') {
+                addMessage('Mr. Botner: ' + message.parts[0].text);
+            }
+            else {
+                addMessage('You: ' + message.parts[0].text);
+            }
+        });
+    });
+}
+
+function sendMessage() {
+    if (seq === 1) {
+        var userInput = document.getElementById('userInput');
+        var message = userInput.value;
+        userInput.value = '';
+        addMessage('Note for Mark: ' + message);
+        fetch('/handleMemo', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({message: message}),
+        }).then(seq = 0).then(addMessage('Mr. Botner: Ok, I\'ll let Mark know. What else can I help you with?'))
+    }
+    else {
+        var userInput = document.getElementById('userInput');
+        var message = userInput.value;
+        if (message.trim() !== '') {
+            addMessage('You: ' + message);
+            userInput.value = '';
+            // Send the message to the server
+            fetch('/chat', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({message: message}),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.type === 2) {
+                        window.location.href = data.redirect_url;
+                    }
+                    addMessage('Mr. Botner: ' + data.response);
+                    if (data.type === 3) {
+                        seq = 1;
+                    }
+                    if (data.type === 4) {
+                        const browser = document.getElementById('github-browser');
+                        const content = document.getElementById('github-content');
+                        Calendly.initInlineWidget({
+                            url: 'https://calendly.com/markbochner1',
+                            parentElement: document.getElementById('calendly-embed')
+                        });
+                        browser.style.display = 'block';
+                    }
+
+                });
+        }
+    }
+}
+
+function addMessage(message) {
+    var chatMessages = document.getElementById('chatMessages');
+    var messageElement = document.createElement('p');
+    messageElement.textContent = message;
+    chatMessages.appendChild(messageElement);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+}
